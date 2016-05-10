2ab5f7086bc2221fe756343c45d1c867b0b4b1a8
Dan LaFeir <dlafeir@thoughtworks.com>
2016-05-10 12:15:16 -0400

    Dan | Seperated react boilerplate from game for testing

--- react/app/app.js
+++ react/app/app.js
@@ -1,25 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import GameBoard from './gameboard'
-
-class TicTacToe extends React.Component {
-  render() {
-    return <div style={styles.container}>
-      <h1 style={styles.header}>Tic Tac Toe Game</h1>
-      <GameBoard/>
-    </div>
-  }
-}
-
-const styles = {
-  container: {
-    margin: '0 auto',
-    width: '500px'
-  },
-  header: {
-    margin: 'auto 0'
-  }
-};
+import TicTacToe from './tictactoe'
 
 ReactDOM.render(<TicTacToe/>, document.getElementById('app'))
---
+++ react/app/tictactoe.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import GameBoard from './gameboard'
+
+class TicTacToe extends React.Component {
+  render() {
+    return <div style={styles.container}>
+      <h1 style={styles.header}>Tic Tac Toe Game</h1>
+      <GameBoard/>
+    </div>
+  }
+}
+
+const styles = {
+  container: {
+    margin: '0 auto',
+    width: '500px'
+  },
+  header: {
+    margin: 'auto 0'
+  }
+};
+
+module.exports = TicTacToe;
